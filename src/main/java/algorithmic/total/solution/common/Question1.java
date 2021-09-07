e405c5fbe5ae7891372ba322a5984635d66e79c9
wangzibin <903328615@qq.com>
2021-09-07 09:48:43 +0800

    init

---
+++ src/main/java/algorithmic/total/solution/common/Question1.java
@@ -0,0 +1,25 @@
+package algorithmic.total.solution.common;
+
+/**
+ * @program: algorithmic-total-solution
+ * @description: 拼 M 面值硬币方法（动态规划问题）
+ * 现在有 n1 + n2 种面值的硬币，n1 种为普通面值硬币，可以随意取用。n2 种为纪念币，每种最多只能取一个。
+ * 每种硬币有一个面值，问：凑满 M 面值有多少种取法？
+ * @author: wangzibin
+ * @create: 2021-09-06
+ **/
+public class Question1 {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[][] getCoin1(int[] arr, int target) {
+        int[][] dp = new int[arr.length][];
+
+        return dp;
+    }
+
+}
+
