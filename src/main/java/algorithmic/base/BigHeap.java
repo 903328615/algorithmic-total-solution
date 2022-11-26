363f53ae17d49ed737e926154821dfeb9016c2fd
wangzibin <wangzibin@kanzhun.com>
2022-11-26 21:52:57 +0800

    Update BigHeap.java

--- src/main/java/algorithmic/base/BigHeap.java
+++ src/main/java/algorithmic/base/BigHeap.java
@@ -1,6 +1,11 @@
 package algorithmic.base;
 
+import util.CommonUtil;
+import util.RandomUtil;
+
 import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 /**
  * @program: algorithmic-total-solution
@@ -11,33 +16,98 @@ import java.util.Arrays;
 public class BigHeap {
 
     private int[] data;
-
     private int heapSize;
-    private int maxSize;
+    private int size;
 
 
     public BigHeap(int size) {
         // 0 位置弃置不用
         data = new int[size + 1];
-        heapSize = 1;
-        maxSize = size + 1;
+        heapSize = 0;
+        this.size = size;
+    }
+
+    public BigHeap(int[] arr) {
+        // 0 位置弃置不用
+        int size = arr.length + 1;
+        data = new int[size];
+        heapSize = arr.length;
+        this.size = size;
+        for (int i = 0; i < arr.length; i++) {
+            data[i + 1] = arr[i];
+        }
+        for (int i = heapSize; i >= 1; i--) {
+            downBalance(i);
+        }
+    }
+
+
+    public void sort() {
+        while (!isEmpty()) {
+            swap(1, heapSize);
+            heapSize--;
+            downBalance(1);
+        }
     }
 
     public void heapInsert(int num) {
-        if (heapSize >= maxSize) {
+        if (heapSize >= size) {
             throw new IllegalStateException("堆已满");
         }
-        int currentIndex = heapSize;
-        data[heapSize++] = num;
+        int currentIndex = ++heapSize;
+        data[currentIndex] = num;
         // 这是一个向上比对交换的过程
+        upBalance(currentIndex);
+
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("empty heap");
+        }
+        int result = data[1];
+        swap(1, heapSize--);
+        downBalance(1);
+        return result;
+    }
+
+    public boolean isEmpty() {
+        return heapSize == 0;
+    }
+
+
+    /**
+     * @Description: 向上平衡
+     * @Param currentIndex
+     * @Return void
+     */
+    private void upBalance(int currentIndex) {
         int parentIndex = findParent(currentIndex);
         while (data[currentIndex] > data[parentIndex] && parentIndex != 0) {
             swap(currentIndex, parentIndex);
             currentIndex = parentIndex;
             parentIndex = findParent(currentIndex);
         }
-
     }
+
+    private void downBalance(int currentIndex) {
+        int left = findLeftChild(currentIndex);
+        // 如果左孩子已经越界，则不需要调整
+        while (left <= heapSize) {
+            int right = findRightChild(currentIndex);
+            int maxIndex = left;
+            if (right <= heapSize) {
+                maxIndex = data[left] > data[right] ? left : right;
+            }
+            if (data[currentIndex] >= data[maxIndex]) {
+                return;
+            }
+            swap(maxIndex, currentIndex);
+            currentIndex = maxIndex;
+            left = findLeftChild(currentIndex);
+        }
+    }
+
 
     private int findLeftChild(int currentIndex) {
         return currentIndex << 1;
@@ -58,14 +128,25 @@ public class BigHeap {
     }
 
     public static void main(String[] args) {
-        BigHeap bigHeap = new BigHeap(20);
-        bigHeap.heapInsert(1);
-        bigHeap.heapInsert(2);
-        bigHeap.heapInsert(3);
-        bigHeap.heapInsert(4);
-        bigHeap.heapInsert(5);
-        bigHeap.heapInsert(6);
-        System.out.println(Arrays.toString(bigHeap.data));
+        for (int i = 0; i < 10000; i++) {
+            int[] arr = RandomUtil.randomArr();
+            int[] arr2 = CommonUtil.copyArr(arr);
+            BigHeap bigHeap = new BigHeap(arr);
+            bigHeap.sort();
+            MargeSort.sort(arr2);
+            for (int j = 0; j < arr.length; j++) {
+                if (arr2[j] != bigHeap.data[j + 1]) {
+                    System.out.println(Arrays.toString(arr2));
+                    System.out.println(Arrays.toString(bigHeap.data));
+                    System.out.println("error");
+                    return;
+                }
+            }
+        }
+        System.out.println("success");
+
+        Queue<Integer> queue = new PriorityQueue<>();
+        queue.add(1);
     }
 
 }
